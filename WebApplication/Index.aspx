4ecd5339de2ab109ac6a980562e6eef99390dce0
Mario Juarez <juarezmario29@yahoo.com>
2020-02-03 10:56:44 -0600

    Insert Sql Command

--- WebApplication/Index.aspx
+++ WebApplication/Index.aspx
@@ -6,8 +6,18 @@
 <head runat="server">
     <title></title>
 </head>
-<body>
+<body style="height: 403px">
+    
+    <form id="form1" runat="server">
     
     <asp:Label ID="lbl_test" runat="server" ></asp:Label>
+&nbsp;
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+    </form>
 </body>
 </html>
--- WebApplication/Index.aspx.cs
+++ WebApplication/Index.aspx.cs
@@ -15,23 +15,38 @@ namespace WebApplication
             //Connect to the DB
             //string connStr = "Server=;Database=BuildingAccess;Integrated Security = True;";
             SqlConnection conn = new SqlConnection(@"Data Source=(local)\sqlexpress;Database=BuildingAccess;Integrated Security = True;");
-            
+
             conn.Open();
 
             //Create a command
-            SqlCommand cmd = new SqlCommand("SELECT [IdentificationCardID],[Name],[OrgStructure],"+
+            //var command
+            //SqlCommand
+            /**SqlCommand cmd = new SqlCommand("SELECT [IdentificationCardID],[Name],[OrgStructure]," +
                 "[PhoneNumber],[EmailAddress],[HireDate],[CardExpireDate],[TerminationDate]," +
                 "[WorkerTypeID],[Company],[CourtAccessRequired],[IDCardNumber],[DepatAbrev]," +
                 "[Department]" +
-                "FROM[dbo].[IdentificationCards]");
+                "FROM[dbo].[IdentificationCards]" +
+                "Where [IDCardNumber] = 99994");**/
+           
+            /**SqlCommand cmd = new SqlCommand("INSERT INTO [dbo].[AccessLogs]" +
+                "(AccessLocationID), (StationID), (AccessDate), (OperatorLogin)" +
+                "Values [WorkerTypeID], [Department], [HireDate], [PhoneNumber]" +
+                "FROM [dbo].[IdentificationCards]" +
+                "Where [IdentificationCardID] = 1");**/
+            SqlCommand cmd = new SqlCommand("INSERT INTO [AccessLogs]" + "([AccessLocationID],[StationID],[AccessDate],[IDCardNumber],[DeclineReason],[OperatorLogin])" +
+                             "Select [WorkerTypeID], [Department], [CardExpireDate], [IDCardNumber], [Name], [OrgStructure]" +
+                             "FROM [IdentificationCards]" +
+                             "Where [IdentificationCardID] = 1");
+    
             cmd.CommandType = System.Data.CommandType.Text;
             cmd.Connection = conn;
-
+            cmd.ExecuteNonQuery();
             string temp = "";
             //Read from DB
             SqlDataReader reader = cmd.ExecuteReader();
             while(reader.Read())
             {
+                
                 temp += reader["IdentificationCardID"].ToString();
                 temp += reader["name"].ToString();
                 temp += reader["PhoneNumber"].ToString();
@@ -39,10 +54,19 @@ namespace WebApplication
                 temp += reader["CardExpireDate"].ToString();
                 temp += "<br/>";
             }
-
             conn.Close();
-
             lbl_test.Text = temp;
+            //conn.Open();
+            //SqlCommand cmd2 = new SqlCommand ("INSERT INTO )
+            //SqlCommand cmd2 = new SqlCommand("INSERT INTO [dbo].[AccessLogs]([AccessLogID])" +
+            //    "Values('ID')");
+            //cmd2.CommandType = System.Data.CommandType.Text;
+            //cmd2.Connection = conn;
+            //cmd2.ExecuteNonQuery();
+
+            //conn.Close();
+            
         }
+
     }
 }
