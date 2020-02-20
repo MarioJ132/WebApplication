f3796d53cf7dc60bcbf4a10fabfad170b8d3853b
Mario <juarezmario29@yahoo.com>
2020-02-20 08:33:36 -0600

    Insert works with subqueries

--- WebApplication/Index.aspx.cs
+++ WebApplication/Index.aspx.cs
@@ -14,57 +14,34 @@ namespace WebApplication
         {
             //Connect to the DB
             //string connStr = "Server=;Database=BuildingAccess;Integrated Security = True;";
-            SqlConnection conn = new SqlConnection(@"Data Source=(local)\sqlexpress;Database=BuildingAccess;Integrated Security = True;");
+            SqlConnection conn = new SqlConnection("Data Source=localhost;Database=BuildingAccess;Integrated Security = True;");
 
             conn.Open();
-
-            //Create a command
-            //var command
-            //SqlCommand
-            /**SqlCommand cmd = new SqlCommand("SELECT [IdentificationCardID],[Name],[OrgStructure]," +
-                "[PhoneNumber],[EmailAddress],[HireDate],[CardExpireDate],[TerminationDate]," +
-                "[WorkerTypeID],[Company],[CourtAccessRequired],[IDCardNumber],[DepatAbrev]," +
-                "[Department]" +
-                "FROM[dbo].[IdentificationCards]" +
-                "Where [IDCardNumber] = 99994");**/
-           
-            /**SqlCommand cmd = new SqlCommand("INSERT INTO [dbo].[AccessLogs]" +
-                "(AccessLocationID), (StationID), (AccessDate), (OperatorLogin)" +
-                "Values [WorkerTypeID], [Department], [HireDate], [PhoneNumber]" +
-                "FROM [dbo].[IdentificationCards]" +
-                "Where [IdentificationCardID] = 1");**/
+            int IDScanned = 99991;
+            string PorF = PassOrFail(IDScanned);
+            DateTime date = DateTime.Now;
             SqlCommand cmd = new SqlCommand("INSERT INTO [AccessLogs]" + "([AccessLocationID],[StationID],[AccessDate],[IDCardNumber],[DeclineReason],[OperatorLogin])" +
-                             "Select [WorkerTypeID], [Department], [CardExpireDate], [IDCardNumber], [Name], [OrgStructure]" +
-                             "FROM [IdentificationCards]" +
-                             "Where [IdentificationCardID] = 1");
-    
+                            "Values ((Select [AccessLocationID] FROM [AccessLocations] Where [AccessLocationID] = 1)," +
+                            "(Select [OrgStructure] FROM [IdentificationCards] Where [IdentificationCardID] = 1)," +
+                            "(@date)," +
+                            "(Select [IDCardNumber] FROM [IdentificationCards] Where [IdentificationCardID] = 1)," + 
+                            "(@passorFail)," +
+                            "(Select [firstName] From [OperatorLogin] Where [firstName] = 'Billy'))");
+            cmd.Parameters.AddWithValue("@date", date);
+            cmd.Parameters.AddWithValue("@passorFail", PorF);
+
             cmd.CommandType = System.Data.CommandType.Text;
             cmd.Connection = conn;
             cmd.ExecuteNonQuery();
-            string temp = "";
-            //Read from DB
-            SqlDataReader reader = cmd.ExecuteReader();
-            while(reader.Read())
-            {
-                
-                temp += reader["IdentificationCardID"].ToString();
-                temp += reader["name"].ToString();
-                temp += reader["PhoneNumber"].ToString();
-                temp += reader["EmailAddress"].ToString();
-                temp += reader["CardExpireDate"].ToString();
-                temp += "<br/>";
-            }
             conn.Close();
-            lbl_test.Text = temp;
-            //conn.Open();
-            //SqlCommand cmd2 = new SqlCommand ("INSERT INTO )
-            //SqlCommand cmd2 = new SqlCommand("INSERT INTO [dbo].[AccessLogs]([AccessLogID])" +
-            //    "Values('ID')");
-            //cmd2.CommandType = System.Data.CommandType.Text;
-            //cmd2.Connection = conn;
-            //cmd2.ExecuteNonQuery();
+        }
 
-            //conn.Close();
+        private string PassOrFail(int IDScanned)
+        {
+            if ()
+            {
+                return "Pass";
+            }
             
         }
 
